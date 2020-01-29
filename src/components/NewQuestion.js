9f77c9e70ac6b3a3130f74747292857e431d9336
yasser-ezzat-abdelhameed <yasseraudio@gmail.com>
2020-01-29 22:11:11 +0200

    update: Added prop-types and hooks for NewQuestion component

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,67 +1,68 @@
 import React from 'react'
-import { withRouter } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 import { handleAddQuestion } from '../actions/questions' 
-class NewQuestion extends React.Component {
-  state = {
-    optionOne: "",
-    optionTwo: ""
-  }
+import { useFormInput } from '../hooks'
 
-  handleAddQuestion = e => {
+const NewQuestion = props => {
+  let history = useHistory()
+  const optionOne = useFormInput("")
+  const optionTwo = useFormInput("")
+
+  const handleAddQuestionSubmit = e => {
     e.preventDefault()
-    const { dispatch, authedUserId, history } = this.props
-    const { optionOne, optionTwo } = this.state
-    dispatch(handleAddQuestion(optionOne, optionTwo, authedUserId, () => { history.push('/') }))
+    const { dispatch, authedUserId } = props
+    dispatch(handleAddQuestion(optionOne.value, optionTwo.value, authedUserId, () => { history.push('/') }))
   }
 
-  checkIfFormIsValid = () => {
-    const { optionOne, optionTwo } = this.state
-    return optionOne && optionTwo
+  const checkIfFormIsValid = () => {
+    return optionOne.value && optionTwo.value
   }
   
-  render() {
-    return (
-      <div className="card">
-        <div className="card-header">
-          <h2>Create New Question</h2>
-        </div>
-        <div className="card-body">
-          <div className="card-content flex-start">
-              <p>Complete the question:</p>
-            
-              <h3 className="margin-top">Would you rather...</h3>
-              <input
-                className="input-field"
-                type="text"
-                placeholder="Please type option one text here"
-                value={this.state.optionOne}
-                onChange={e => this.setState({ optionOne: e.target.value })}
-              />
-              <div className="options-separator">
-                <div className="line flex-3"></div>
-                <div className="flex-1 center bold">OR</div>
-                <div className="line flex-3"></div>
-              </div>
-              <input
-                className="input-field"
-                type="text"
-                placeholder="Please type option two text here"
-                value={this.state.optionTwo}
-                onChange={e => this.setState({ optionTwo: e.target.value })}
-              />
-              <button 
-                className={"button button-submit" + (this.checkIfFormIsValid() ? "" : " disabled")}
-                disabled={!this.checkIfFormIsValid()}
-                onClick={this.handleAddQuestion}
-              >Submit</button>
-          </div>
+  return (
+    <div className="card">
+      <div className="card-header">
+        <h2>Create New Question</h2>
+      </div>
+      <div className="card-body">
+        <div className="card-content flex-start">
+            <p>Complete the question:</p>
+          
+            <h3 className="margin-top">Would you rather...</h3>
+            <input
+              className="input-field"
+              type="text"
+              placeholder="Please type option one text here"
+              {...optionOne}
+            />
+            <div className="options-separator">
+              <div className="line flex-3"></div>
+              <div className="flex-1 center bold">OR</div>
+              <div className="line flex-3"></div>
+            </div>
+            <input
+              className="input-field"
+              type="text"
+              placeholder="Please type option two text here"
+              {...optionTwo}
+            />
+            <button 
+              className={"button button-submit" + (checkIfFormIsValid() ? "" : " disabled")}
+              disabled={!checkIfFormIsValid()}
+              onClick={handleAddQuestionSubmit}
+            >Submit</button>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 const mapStateToProps = ({ authedUser }) => ({ authedUserId: authedUser.id })
 
-export default connect(mapStateToProps)(withRouter(NewQuestion))
+NewQuestion.propTypes = {
+  authedUserId: PropTypes.string.isRequired,
+  dispatch: PropTypes.func.isRequired,
+}
+
+export default connect(mapStateToProps)(NewQuestion)
